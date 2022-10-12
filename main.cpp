86f9593023384556ed4e1581029be6f45cf3efd8
thebaolin <bchang916@gmail.com>
2022-10-12 18:30:35 -0400

    still need to fix vigenere.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "caesar.cpp"
+#include "vigenere.cpp"
 using namespace std;
 
 int main()
 {
     cout<<encryptCaesar("Way to Go!", 5)<<endl;
-  //cout<< encryptVigenere("Hello, World!", "cake") << endl; //Jevpq, Wyvnd!
+    cout<< encryptVigenere("Hello, World!", "cake") << endl; //Jevpq, Wyvnd!
   return 0;
 }
--- vigenere.cpp
+++ vigenere.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include "caesar.h"
-#include <caesar.cpp"
 #include "vigenere.h"
 using namespace std;
 
 string encryptVigenere(string plaintext, string keyword){
     string newtext = "";
-    for(int i = 0; i < plaintext.length(); i++){
-        for(int j = 0; j < keyword.length(); j++){
-            int keyword_shifter = ((int)keyword[j]) - 1;
-            newtext += encryptCaesar(plaintext, keyword_shifter);
+    bool finished = false;
+    int counter = 0;
+
+    while(!finished){
+    for(int i = 0; i < keyword.length(); i++){
+        if(counter == plaintext.length())
+            finished = true;
+        else{
+            int keyword_shifter = ((int)keyword[i] - 1);
+            newtext += shiftChar(plaintext[i], keyword_shifter);
+            counter++;
+            }
         }
-    }  
+    }
     return newtext;
 }
+
+/*
+string encryptVigenere(string plaintext, string keyword){
+    string newtext = "";
+    bool finished = false;
+    int keyword_counter = 0; 
+
+    for(int i = 0; i < keyword.length(); i++){
+        if(isalpha(keyword[i]){
+
+            keyword_counter++;
+            if(keyword_counter == keyword.length())
+                counte
+        }
+    }
+}
+
+*/
